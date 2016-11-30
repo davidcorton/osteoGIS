85df63a568248d2e0cb088643b648b829a43274a
David Orton <david.orton@gmail.com>
2016-11-30 23:04:29 +0000

    Started to add description text

--- R/skelMap.R
+++ R/skelMap.R
@@ -1,15 +1,33 @@
+#' Plot colour-graded skeletal diagram.
+#'
+#' A wrapper for raster::spplot. Plots single column from input data frame on one of the built-in skeletal shapefiles.
+#' @param data Data frame with at least one numeric or integer column containing data to be plotted,
+#'    and at least one field corresponding to the element names or codes used in the shapefile attribute table.
+#' @param plot.field Character. The name of the field in 'data' containing the data to be plotted. Defaults to "NISP"
+#' @param join.field Character. The name of the field in 'data' that should be used to link to the shapefile attribute table. Defaults to "El_code"
+#' @param template Character. The name of the skeletal template to be used. This should be an .Rda file in the package "data" directory. Built-in
+#'    options are "Bos", "Ovis", "Equus", "Cervus", "Canis", "Sus". Defauls to "Bos".
+#' @param background Logical: should a silhouette of the animal be plotted in the background? Defaults to TRUE.
+#' @param details Logical: should details of bone features be drawn, as well as the element outlines? Defaults to TRUE.
+#' @param bg Character. Colour for background silhouette, if 'background' is TRUE. Defaults to "gray80".
+#' @param null.col. Colour for elements with missing data. Defaults to "white".
+#' @return None.
+#' @export
+#' @examples
+#' egData <- data.frame(El_code = c(1:140), NISP = rep(1:2, 70))
+#' skelMap(egData)
 
 skelMap <- function(data, plot.field="NISP", join.field="El_code", template="Bos", background=T, details=T, bg="gray80", null.col="white") {
       require(raster)
       require(maptools)
-      elements <- readShapePoly(file.path(template, "elements.shp"))
+      elements <- readShapePoly(file.path("data", template, "elements.shp"))
       layers <- list()
       if(details==T) {
-            dets <- list(readShapeLines(file.path(template, "details.shp")), first=F)
+            dets <- list(readShapeLines(file.path("data", template, "details.shp")), first=F)
             layers[[length(layers)+1]] <- dets
       }
       if(background==T) {
-            backgr <- list(readShapePoly(file.path(template, "background.shp")), fill=bg, first=T)
+            backgr <- list(readShapePoly(file.path("data", template, "background.shp")), fill=bg, first=T)
             layers[[length(layers)+1]] <- backgr
       }
       layers[length(layers)+1] <- list(list(elements, fill=null.col, first=T))
